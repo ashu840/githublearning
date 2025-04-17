07ae072dcc16b46d6fdff505962ea9df6ab66f95
Alok <alokprakash731@gmail.com>
2025-04-17 11:32:50 +0530

    first commit done

---
+++ github/src/test/java/gitpractice/GitPractice.java
@@ -0,0 +1,11 @@
+package gitpractice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GitPractice {
+@Test
+public void git() {
+	Reporter.log("git practice",true);
+}
+}
