88451e4cd16f6348fa4ad17980bd50b6e97786c4
Ashutosh Dwivedi <ashutoshdwivediashu98@gmail.com>
2025-04-17 11:14:37 +0530

    New Commit

---
+++ github/src/test/java/demo/NewPullRequest.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class NewPullRequest {
+	
+	@Test
+	
+	public void newMethod() {
+		Reporter.log("Hello World..",true);
+	}
+
+}
