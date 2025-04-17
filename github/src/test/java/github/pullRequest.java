ac3343232083f7654a8bd69a759250be211ec38e
tanutripathi <tanutripathi@mac>
2025-04-17 10:58:04 +0530

    first commit

---
+++ github/src/test/java/github/pullRequest.java
@@ -0,0 +1,11 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class pullRequest {
+@Test
+public void gitHub() {
+	Reporter.log("pull request",true);
+}
+}
